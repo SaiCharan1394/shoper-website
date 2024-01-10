b955a9f0d4f88847f6a7e1bcfd9eec28945cd3fa
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2024-01-10 18:38:26 +0530

    first commit

---
+++ src/components/CartItems/CartItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./CartItems.css"
+const CartItems = () => {
+  return (
+    <div className=''></div>
+  )
+}
+
+export default CartItems
---
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./DescriptionBox.css"
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium excepturi dolorum vitae cumque numquam reiciendis., et hic voluptas soluta atque ea dolorem temporibus, voluptate recusandae dolor consectetur? Sit, numquam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laudantium excepturi dolorum vitae cumque numquam reiciendis., et hic voluptas soluta atque ea dolorem temporibus, voluptate recusandae dolor consectetur? Sit, numquam.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
